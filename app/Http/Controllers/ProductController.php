ede5a699dd68a59fb31a7089be1d58b105014e45
ajins15 <ansengarie@gmail.com>
2021-10-31 11:44:02 +0700

    CRUD product delete

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Support\Str;
-use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use App\Http\Requests\ProductRequest;
 use Yajra\DataTables\Facades\DataTables;
@@ -24,16 +23,16 @@ class ProductController extends Controller
             return DataTables::of($query)
                 ->addColumn('action', function ($item) {
                     return '
-                        <a href="' . route('dashboard.product.edit', $item->id) . '" class="bg-gray-500 text-white rounded-md px-2 py-1 m-2">
+                        <a class="inline-block border border-gray-700 bg-gray-700 text-white rounded-md px-2 py-1 m-1 transition duration-500 ease select-none hover:bg-gray-800 focus:outline-none focus:shadow-outline" 
+                            href="' . route('dashboard.product.edit', $item->id) . '">
                             Edit
                         </a>
-                        <form method="POST" class="inline-block" action"' . route('dashboard.product.destroy', $item->id) . '">
-                            <button class="bg-red-500 text-white rounded-md px-2 py-1 m-2">
-                                Hapus
-                            </button>
-                            '. method_field('delete') . csrf_field() .'
-                        </form>
-                    ';
+                        <form class="inline-block" action="' . route('dashboard.product.destroy', $item->id) . '" method="POST">
+                        <button class="border border-red-500 bg-red-500 text-white rounded-md px-2 py-1 m-2 transition duration-500 ease select-none hover:bg-red-600 focus:outline-none focus:shadow-outline" >
+                            Hapus
+                        </button>
+                            ' . method_field('delete') . csrf_field() . '
+                        </form>';
                 })
                 ->editColumn('price', function ($item) {
                     return number_format($item->price);
